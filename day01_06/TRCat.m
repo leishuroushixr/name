d0f52ec79dbf65a88376957a7c1a0f5506fbeb75
woodpower <tarenat@XR.local>
2016-05-16 19:36:36 +0800

    day01_06 day01_06.xcodeproj

---
+++ day01_06/TRCat.m
@@ -0,0 +1,25 @@
+//
+//  TRCat.m
+//  day01_06
+//
+//  Created by tarenat on 16/3/25.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import "TRCat.h"
+
+@implementation TRCat
+-(void)setName:(NSString *)name
+{
+    _name = name;
+}
+-(NSString *)name
+{
+    return  _name;
+}
+-(void)bark
+{
+    NSLog(@"猫咪%@喵喵的叫",_name);
+}
+
+@end
---
+++ day01_06/TRCat1.h
@@ -0,0 +1,18 @@
+//
+//  TRCat1.h
+//  day01_06
+//
+//  Created by tarenat on 16/3/25.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRCat1 : NSObject
+{
+    NSString *name;
+}
+-(void)setName1:(NSString*)name;
+-(NSString*)name;
+-(void)bark;
+@end
